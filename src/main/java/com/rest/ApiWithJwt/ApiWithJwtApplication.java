5f5bfd9753bc7635ab532b573e6364009c400ef1
erhanorun <erhanorun@outlook.com>
2023-09-13 12:30:43 +0300

    initial commit

---
+++ src/main/java/com/rest/ApiWithJwt/ApiWithJwtApplication.java
@@ -0,0 +1,13 @@
+package com.rest.ApiWithJwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiWithJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiWithJwtApplication.class, args);
+	}
+
+}
